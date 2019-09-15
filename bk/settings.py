11cee6d7f29fdd800f905770cc3256fd99cab339
letsgo247 <letsgo247@hanmail.net>
2019-09-16 00:26:03 +0900

    Final

--- bk/settings.py
+++ bk/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4=za-!#q29+w4gp0%#2q-(9x_w$v58$f0zso%q=w1j%a$aii^m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'www.breakorean.kr', 'breakorean.kr',]
 # Archive ['.pythonanywhere.com',]
