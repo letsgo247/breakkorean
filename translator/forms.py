d8cbccec0af8554b4e7d9b8e7aff873cc0609572
letsgo247 <letsgo247@hanmail.net>
2019-09-16 00:10:26 +0900

    Form

--- translator/forms.py
+++ translator/forms.py
@@ -1,9 +1,15 @@
 from django import forms
+from django.forms import Textarea
+
 
 from .models import Inputs
 
 class InputForm(forms.ModelForm):
     # text = forms.CharField(label='')
+    text = forms.CharField(widget=forms.Textarea, label='')
     class Meta:
         model = Inputs
         fields = ('text',)
+        # widgets = {
+        #     'text': Textarea(attrs={'cols': 20, 'rows': 20}),
+        # }
