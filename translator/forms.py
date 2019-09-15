87a7c2d83acb5a71a5e1d34c2d7e12110ce68d1e
letsgo247 <letsgo247@hanmail.net>
2019-09-15 21:57:25 +0900

    alg1

--- translator/forms.py
+++ translator/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from .models import Inputs
 
 class InputForm(forms.ModelForm):
+    # text = forms.CharField(label='')
     class Meta:
         model = Inputs
         fields = ('text',)
