e5ef48ad2f835eba121529098a546f271d4d001a
letsgo247 <letsgo247@hanmail.net>
2019-09-15 13:09:47 +0900

    Breakorean.kr

--- bk/settings.py
+++ bk/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = '4=za-!#q29+w4gp0%#2q-(9x_w$v58$f0zso%q=w1j%a$aii^m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'www.breakorean.kr', 'breakorean.kr',]
-
+ALLOWED_HOSTS = ['127.0.0.1', 'www.breakorean.kr', 'breakorean.kr',]
+# Archive ['.pythonanywhere.com',]
 
 # Application definition
 
