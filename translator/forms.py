53d4a28f56c51c9c96ef3e469418be1fd9fa7b04
letsgo247 <letsgo247@hanmail.net>
2019-09-17 00:32:39 +0900

    v1.02

--- translator/forms.py
+++ translator/forms.py
@@ -3,7 +3,7 @@ from .models import Inputs
 
 class InputForm(forms.ModelForm):
     # text = forms.CharField(label='')
-    text = forms.CharField(widget=forms.Textarea(attrs={'rows': 10, 'cols': 30}), label='')
+    text = forms.CharField(initial='문장을 입력하세요', widget=forms.Textarea(attrs={'rows': 10, 'cols': 30}), label='')
     class Meta:
         model = Inputs
         fields = ('text',)
