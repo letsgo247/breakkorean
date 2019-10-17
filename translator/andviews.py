9c5d24b63058308e078e2db525a16e332251c186
letsgo247 <letsgo247@hanmail.net>
2019-10-17 22:26:43 +0900

    WebAnd

---
+++ translator/andviews.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Inputs
+from .forms import InputForm
+
+from .algorithm import alg1, alg2, alg3
+
+def cover(request):
+    return render(request, 'translator/andcover.html')
+
+
+def select(request):
+    return render(request, 'translator/andselect.html')
+
+
+def translator(request, alg_id):
+    alg_id=str(alg_id)
+    if request.method == "POST":
+        form = InputForm(request.POST)
+        #print(request.POST)
+        if form.is_valid():
+            form.save()
+            a='/andtranslated'+alg_id
+            return redirect(a)
+    else:
+        form = InputForm()
+    return render(request, 'translator/andtranslator'+alg_id+'.html',{'form':form})
+
+
+def translated(request, alg_id):
+    alg_id=str(alg_id)
+    content = Inputs.objects.last()
+
+    if alg_id=='1':
+        output = alg1(str(content))
+    if alg_id=='2':
+        output = alg2(str(content))
+    if alg_id=='3':
+        output = alg3(str(content))
+
+    return render(request, 'translator/andtranslated'+alg_id+'.html', {'output':output})
--- translator/urls.py
+++ translator/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from . import views
+from . import views, andviews
 
 urlpatterns = [
     path('', views.cover, name='cover'),
     path('select', views.select, name='select'),
     path('translator<int:alg_id>', views.translator, name='translator'),
     path('translated<int:alg_id>', views.translated, name='translated'),
+
+    path('and', andviews.cover, name='andcover'),
+    path('andselect', andviews.select, name='andselect'),
+    path('andtranslator<int:alg_id>', andviews.translator, name='andtranslator'),
+    path('andtranslated<int:alg_id>', andviews.translated, name='andtranslated'),
 
 ]
